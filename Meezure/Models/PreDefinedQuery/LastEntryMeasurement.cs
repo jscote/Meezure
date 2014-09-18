eb90489390c80bbdbeafec55d0be2d48a1fb916e
Jean-Sebastien Cote <jean.sebastien.cote@gmail.com>
2014-09-17 22:56:44 -0700

    Fix bug where sometimes, the last entry was not returned. It was due to picking up the max date for the right subject but wrong definitiion

--- Meezure/Models/PreDefinedQuery/LastEntryMeasurement.cs
+++ Meezure/Models/PreDefinedQuery/LastEntryMeasurement.cs
@@ -11,7 +11,7 @@ namespace Meezure
 
 		public override MeasurementInstanceModel Query (object[] parameters)
 		{
-			string selectStr = "SELECT t1.* FROM MeasurementInstanceModel AS t1 WHERE t1.MeasurementSubjectId == ? AND t1.DateRecorded ==  (SELECT MAX(t2.DateRecorded)  FROM MeasurementInstanceModel AS t2 WHERE t2.MeasurementSubjectId == t1.MeasurementSubjectId) AND t1.MeasurementDefinitionId == ?";
+			string selectStr = "SELECT t1.* FROM MeasurementInstanceModel AS t1 WHERE t1.MeasurementSubjectId == ? AND t1.DateRecorded ==  (SELECT MAX(t2.DateRecorded)  FROM MeasurementInstanceModel AS t2 WHERE t2.MeasurementSubjectId == t1.MeasurementSubjectId AND t2.MeasurementDefinitionId == t1.MeasurementDefinitionId limit 1) AND t1.MeasurementDefinitionId == ?";
 		
 			var data = _repository.GetAllWithChildren (selectStr, parameters);
 
